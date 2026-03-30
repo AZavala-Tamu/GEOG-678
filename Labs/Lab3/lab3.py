85185d29d35ca49884cf901576711a9bc556e864
Alexandra Zaval <aez24@tamu.edu>
2026-03-30 17:59:21 -0500

    Add Changes to Lab 3

---
+++ Labs/Lab3/lab3.py
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+      <title> Alexandra Zavala Resume </title>
+      <style>
+        .description {
+          font-size: 20px;font-style: italic;
+          text-align: center;
+          }
+        body {
+          font-family: Arial, sans-serif;
+          margin: 30px;
+          }
+          
+        h1 {
+          color: darkgreen;
+          text-align: center;
+          }
+          
+        h3, h4 {
+          color: seagreen;
+          }
+        
+        #contact {
+          text-align: center;margin-top: 30px; padding: 20px;border-top: 2px  solid seagreen;
+          }
+        
+
+        #contact p {
+          margin: 5px 0;
+          }
+          
+        
+      </style>
+
+      
+    </head>
+
+    <body>
+      <h1> Alexandra Zavala</h1>
+      <p class = 'description'> While the majority of my experience is surrounded around animals, I have learned a lot about buisness management from my experience as an operation manager and am open to learning new things at every job I have!</p>
+
+<!--Education listed -->
+      <div id = 'education'>
+        <h3>Education</h3>
+        <ul>
+
+          <li><strong>Completed: </strong></li>
+          <p>Bachelors Degree, Texas State University's <a href = "https://www.owls.txst.edu/about-us/mission.html" target = "_blank"> OWLS Program </a></p>
+
+          <li><strong> In Progress: </strong></li>
+          <p>Master Degree, Texas A&M University's <a href = "https://catalog.tamu.edu/graduate/colleges-schools-interdisciplinary/arts-and-sciences/geography/mgsc/#programrequirementstext" target = "_blank">Geoscience</a></p>
+
+        </ul>
+       </div> 
+
+<!-- work experience, list of experience -->
+      <div id = 'experience'>
+        <h3> Work Experience</h3>
+        <ul>
+          <li><strong>Bobbi Colorado's Canine Camp</strong>,<em> Manager</em>: A dog boarding a training facility with 10+ staff members and anywhere between 10 and 120 dogs at a time. </li>
+          <li><strong>Bobbi Colorado and The Wildbunch</strong>, <em> Animal Wrangler & Animal Coordinator</em>: Provider of animal work for film, commercials, and photoshoots. Anything from Ants to Elephants for small indenpent shoots, or big blockbusters.</li>
+        </ul>
+      </div>
+
+<!--Skills Section -->
+      <div id = 'skills'>
+        <h3>Skills</h3>
+
+       <p>The skills listed here are a combination of all of the previous experience I've had in the previously mentioned 'work experience' area:</p>
+
+       <ul>
+        <li> Proficient in Quickbooks </li>
+        <li> Works well under pressure</li>
+        <li> Advanced knowledge in Microsoft Word</li>
+        <li> Experienced in Microsoft Excel</li>
+        <li> Beginner understanding of Python</li>
+        <li> Advanced communication skills</li>
+        <li> Time management skills</li>
+        <li> Vast knowledge on different animal care requirments</li>
+
+       </ul>
+      </div>
+
+<!-- Contact info  -->
+      <div id = 'contact'>
+        <h4>Contact Info</h4>
+          <p><strong> Phone number:</strong> (512) 361-8990</p>
+          <p><strong>Email:</strong> alexandra030201@gmail.com</p>
+          <p><strong> Address:</strong> 1007s Congress Ave, Apt 817, Austin Tx, 78704 
+          </p>
+      </div>
+    </body>
+</html>
